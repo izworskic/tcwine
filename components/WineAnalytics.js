4fc11837d246a50302357eeba00fb5b14376f8c3
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-08-29 14:51:45 -0400

    Track winery and event detail landings

--- components/WineAnalytics.js
+++ components/WineAnalytics.js
@@ -8,7 +8,11 @@ export default function WineAnalytics() {
   const pathname = usePathname();
 
   useEffect(() => {
-    const landing = WINE_LANDING_KEYS[pathname] ?? "other";
+    const landing =
+      WINE_LANDING_KEYS[pathname] ??
+      (pathname.startsWith("/winery/") ? "winery_detail" :
+       pathname.startsWith("/events/") ? "wine_event_detail" :
+       "other");
     trackWineEvent("wine_landing_viewed", { landing });
   }, [pathname]);
 
