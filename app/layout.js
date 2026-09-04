3019d0449b77866e10602d1fd2c84d17aef8dcaf
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-04 07:20:06 -0400

    Add GA4 to all TC Wine pages

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import Script from "next/script";
 import "./globals.css";
 import WineAnalytics from "@/components/WineAnalytics";
 
+const GA_MEASUREMENT_ID = "G-Y5D2V2W7HN";
+
 export const metadata = {
   metadataBase: new URL("https://tcwine.chrisizworski.com"),
   title: "Traverse City Winery Map & Wine Tour Planner | 40 Wineries",
@@ -31,6 +33,18 @@ export default function RootLayout({ children }) {
         <link rel="author" href="https://chrisizworski.com/chris-izworski/" />
       </head>
       <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <Script id="wine-analytics-queue" strategy="beforeInteractive">
           {`window.va=window.va||function(){(window.vaq=window.vaq||[]).push(arguments)};`}
         </Script>
